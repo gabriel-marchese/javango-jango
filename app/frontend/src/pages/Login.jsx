80407691d569230e0df19b0d21a9964986daf8f4
Gabriel M <gabriel.marchese2@gmail.com>
2023-11-21 13:06:36 -0300

    Tela /login Done

--- app/frontend/src/pages/Login.jsx
+++ app/frontend/src/pages/Login.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import '../styles/pages/login.css'
 
 const Login = () => {
 
   return (
-    <>
-     <h1>Hello World</h1>
-    </>
+    <div className='login-container'>
+      <div className='login-content'>
+        <div className='teste'>
+          <h2>Login</h2>
+          <form action="post">
+            <label htmlFor="">
+              <p className='email-label'>Email</p>
+              <input className='input-login' type="text" />
+            </label>
+
+            <label htmlFor="">
+              <p className='password-label'>Senha</p>
+              <input className='input-login' type="password" />
+            </label>
+
+            <button>Entrar</button>
+          </form>
+        </div>
+        <div className='login-image'>
+        </div>
+      </div>
+    </div>
   );
 };
 
